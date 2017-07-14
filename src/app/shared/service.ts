1d5c5cc562495e81f99d681b7e8fe7782f7f2368
Gasser <Grant_Gasser@Dell.com>
2017-07-14 05:03:16 -0500

    add Hayden requests

--- src/app/shared/service.ts
+++ src/app/shared/service.ts
@@ -4,16 +4,47 @@ import { Http } from '@angular/http';
 @Injectable()
 
 export class Service {
-    private baseUrl = 'https://myapp-dbd8e.firebaseio.com';
+    //private lat = 30.476;
+    //private long = -97.6711;
+    //private store_uri = 'https://api.bestbuy.com/v1/stores((area(' + this.lat + ',' + this.long + ',0.1)))?apiKey=dqyrzm8d558mmdvgjnbhc74m&show=storeId&format=json';
 
     constructor(private http: Http){
 
     }
 
-    getProducts(){
+        getStore(lat, lng){
+        let store_uri = 'https://api.bestbuy.com/v1/stores((area(' + lat + ',' + lng + ',3)))?apiKey=dqyrzm8d558mmdvgjnbhc74m&show=storeId&format=json';
+        return new Promise(resolve => {
+            this.http.get(`${store_uri}`)
+            .subscribe(res => resolve(res.json()));
+        })
+    }
+
+    getDellProds() {
+      return new Promise(resolve => {
+        this.http.get('https://api.bestbuy.com/v1/products(manufacturer=dell&inStoreAvailability=true)?apiKey=dqyrzm8d558mmdvgjnbhc74m&sort=name.dsc&show=name,longDescription,salePrice,percentSavings,condition,features.feature,customerReviewAverage,customerReviewCount,sku&pageSize=100&format=json')
+          .subscribe(res => resolve(res.json()));
+      })
+    }
+
+    getMoreProds(page) {
+      return new Promise(resolve => {
+        this.http.get('https://api.bestbuy.com/v1/products(manufacturer=dell&inStoreAvailability=true)?apiKey=dqyrzm8d558mmdvgjnbhc74m&sort=name.dsc&show=name,longDescription,salePrice,percentSavings,condition,features.feature,customerReviewAverage,customerReviewCount,sku&pageSize=100&page=' + page + 'format=json')
+          .subscribe(res => resolve(res.json()));
+      })
+    }
+
+    getStoresWithProd(product, lat, lng) {
+      return new Promise(resolve => {
+        this.http.get('https://api.bestbuy.com/v1/stores(area(' + lat + ',' + lng + ',3))+products(sku=' + product.sku + ')?format=json&show=storeId,storeType,city,region,name,products.name,products.sku,products&pageSize=1&apiKey=dqyrzm8d558mmdvgjnbhc74m')
+          .subscribe(res => resolve(res.json()));
+      })
+    }
+
+    /*getProducts(){
         return new Promise(resolve => {
             this.http.get(`${this.baseUrl}/products.json`)
             .subscribe(res => resolve(res.json()));
         })
-    }
+    }*/
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,10 +19,12 @@ export class HomePage {
   myInput: string;
   browser: any;
   location: { lat: number, lng: number };
+  google: any;
+  promises: any;
 
   constructor(private nav: NavController, private myService: Service, private navParams: NavParams,
               private iab: InAppBrowser, private geolocation: Geolocation) {
-  
+            console.log('CONSTRUCTOR');
   }
 
 getLocation(){
@@ -34,13 +36,71 @@ getLocation(){
     });
   }
 
+ /*initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new this.google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new this.google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+*/
 
   itemTapped(event, product){
     this.nav.push(ProductInfoPage, product);
   }
 
   ionViewDidLoad(){
-        this.myService.getProducts().then(data => this.products = data);
+        //this.myService.getProducts().then(data => this.products = data);
+        this.myService.getDellProds().then(data => {
+          let ret = <any>{};
+          ret = data;
+          console.log(ret);
+          var numPages = ret.totalPages;
+          console.log(numPages);
+          var products = [];
+          for (var i = 0; i < ret.products.length; i++) {
+            products.push(ret.products[i]);
+          }
+          console.log(products[0].sku.toString());
+          console.log(products);
+          this.myService.getStore(this.location.lat, this.location.lng).then(data => {
+            console.log(data);
+            var promises = [];
+            for (var j = 0; j < products.length; j++) {
+              var product = products[j];
+              //console.log(product);
+              promises.push(this.myService.getStoresWithProd(product, this.location.lat, this.location.lng));
+              var start = new Date().getTime();
+              while (new Date().getTime() < start + 1000);
+            }
+            Promise.all(promises).then(values => {
+              console.log(values);
+              var skus_in_stock = [];
+              for (var k = 0; k < values.length; k++) {
+                if (values[k].stores.length != 0) {
+                  var pos = values[k].canonicalUrl.search("sku=");
+                  var sku_num = values[k].canonicalUrl.substr(pos + 4, 7);
+                  skus_in_stock.push(sku_num);
+                }
+              }
+              console.log(skus_in_stock);
+              var prods_in_stock = [];
+              for (var l = 0; l < products.length; l++) {
+                for (var m = 0; m < skus_in_stock.length; m++) {
+                  if (skus_in_stock[m] === products[l].sku.toString()) {
+                    prods_in_stock.push(products[l]);
+                  }
+                }
+              }
+              console.log(prods_in_stock);
+              this.products = prods_in_stock;
+            });
+          });
+        });
   }
 
   toProductInfo(event, product){
